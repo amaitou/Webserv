19f8198866de01b8bec72b6e1df9d6d10fb7421e
Amine Ait Ouazghour <amineotaku239@gmail.com>
2024-03-27 04:58:32 +0000

    removed binary ata enum

--- includes/HTTP_Request.hpp
+++ includes/HTTP_Request.hpp
@@ -6,7 +6,7 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 05:24:11 by amait-ou          #+#    #+#             */
-/*   Updated: 2024/03/27 02:27:25 by amait-ou         ###   ########.fr       */
+/*   Updated: 2024/03/27 04:58:16 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,6 @@ typedef enum e_post_content_type
 {
 	CHUNKED_TRANSFER_ENCODING,
 	MULTIPART_FORM_DATA,
-	BINARY_DATA,
 	CUSTOM_DATA,
 	_NONE
 }	t_content_type;
