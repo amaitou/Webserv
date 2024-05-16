0d25e16ef0f19ddf8d494fff86d0983a9e897f74
Root-07 <ael-amin@student.1337.ma>
2024-05-16 14:15:33 +0100

    CGI

---
+++ CgiHandler/CGI_Handler.cpp
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CGI_Handler.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-amin <ael-amin@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/02 18:09:55 by ael-amin          #+#    #+#             */
+/*   Updated: 2024/05/16 13:21:53 by ael-amin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "CGI_Handler.hpp"
+
+
+CgiHandler::CgiHandler(const std::string& path) : cgiPath(path) {}
+
+CgiHandler::~CgiHandler() {}
+
+void CgiHandler::executeCgi(const std::string& scriptPath, const std::string& requestMethod, const std::string& requestBody)
+{
+	(void) requestMethod;
+
+	int pipeIn[2];
+	int pipeOut[2];
+	pid_t pid;
+	char buffer[4096];
+	int status;
+
+	if (pipe(pipeIn) == -1 || pipe(pipeOut) == -1) {
+		std::cerr << "Error: pipe failed" << std::endl;
+		exit(1);
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		std::cerr << "Error: fork failed" << std::endl;
+		exit(1);
+	}
+	if (pid == 0)
+	{
+		close(pipeIn[1]);
+		close(pipeOut[0]);
+		dup2(pipeIn[0], 0);
+		close(pipeIn[0]);
+		dup2(pipeOut[1], 1);
+		close(pipeOut[1]);
+
+
+		std::vector<const char*> args;
+		args.push_back(scriptPath.c_str());
+		args.push_back(NULL);
+
+		std::vector<const char*> env;
+		env.push_back(NULL);
+
+		execve(scriptPath.c_str(), (char* const*)args.data(), (char* const*)env.data());
+		std::cerr << "Error: execve failed" << std::endl;
+		exit(1);
+	}
+	else
+	{
+		close(pipeIn[0]);
+		close(pipeOut[1]);
+		write(pipeIn[1], requestBody.c_str(), requestBody.size());
+		close(pipeIn[1]);
+		waitpid(pid, &status, 0);
+		while (read(pipeOut[0], buffer, 4096) > 0)
+		{
+			std::cout << buffer;
+		}
+		close(pipeOut[0]);
+		}
+	}
+
+void CgiHandler::handleRequest(const std::string& method, const std::string& url, const std::string& body)
+{
+	std::string extension = url.substr(url.find_last_of(".") + 1);
+
+	if (extension == "py" || extension == "php" )
+	{
+		std::string scriptPath = cgiPath + url;
+		executeCgi(scriptPath, method, body);
+	}
+}
+
---
+++ CgiHandler/CGI_Handler.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CGI_Handler.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-amin <ael-amin@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/05 11:40:26 by ael-amin          #+#    #+#             */
+/*   Updated: 2024/05/16 13:22:30 by ael-amin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CGI_Handler_HPP
+#define CGI_Handler_HPP
+
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <vector>
+#include <sstream>
+
+
+class CgiHandler {
+private:
+	std::string cgiPath;
+	std::string responseBody;
+	std::string requestBody;
+
+	void executeCgi(const std::string& scriptPath, const std::string& requestMethod, const std::string& requestBody);
+
+public:
+	CgiHandler(const std::string& cgiPath);
+	~CgiHandler();
+	void handleRequest(const std::string& method, const std::string& url, const std::string& body);
+};
+
+#endif
