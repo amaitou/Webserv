1afbe8a4b6ea608757f7aaf28645889d03c3e3c5
Reda LABBIZ <redalabbiz@gmail.com>
2024-05-18 11:26:54 +0100

    handle error in main

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: rlabbiz <rlabbiz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 03:45:06 by amait-ou          #+#    #+#             */
-/*   Updated: 2024/05/15 21:32:22 by amait-ou         ###   ########.fr       */
+/*   Updated: 2024/05/18 11:26:27 by rlabbiz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ int main(int ac, char **ag)
 		int checkError = 0;
 		std::cout << GREY << "[.] " << RESET << "Parsing configuration file..." << RESET << std::endl;
 		std::vector<Config>	servers = getConfig(ag[1], &checkError);
+		if (checkError)
+			return ;
 		std::cout << YELLOW << "[+] " << RESET << "Configuration file was parsed." << RESET << std::endl;
 		TCP_Connection HTTP(servers);
 		HTTP.serve();
