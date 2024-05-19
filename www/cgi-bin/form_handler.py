4da781662a4a00509a6d5152056b1a12c562906e
Root-07 <ael-amin@student.1337.ma>
2024-05-19 13:14:57 +0100

    cgi-bin

---
+++ www/cgi-bin/form_handler.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import cgi
+import cgitb
+
+cgitb.enable()  # Enable CGI error reporting
+
+form = cgi.FieldStorage()
+
+name = form.getvalue("name")
+
+print("Content-Type: text/html")
+print()
+print("<html><head>")
+print("<title>Form Handler</title>")
+print("</head><body>")
+if name:
+    print(f"<h1>Hello, {name}!</h1>")
+else:
+    print("<h1>Please enter your name.</h1>")
+print("</body></html>")
+
